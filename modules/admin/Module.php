8d4a9bde4509ccf4eba853cb3e09e5c857ab1e3e
Виталий <wental-@mail.ru>
2020-12-10 23:37:20 +0300

    Completed admin panel hiding

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -2,6 +2,9 @@
 
 namespace app\modules\admin;
 
+use Yii;
+use yii\filters\AccessControl;
+
 /**
  * admin module definition class
  */
@@ -26,4 +29,27 @@ class Module extends \yii\base\Module
 
         // custom initialization code goes here
     }
+
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'denyCallback' => function ($rule, $action)
+                {
+                    throw new \yii\web\NotFoundHttpException();
+                },
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'matchCallback' => function ($rule, $action)
+                        {
+                            return Yii::$app->user->identity->isAdmin;
+                        }
+                    ],
+                ],
+            ]
+        ];
+    }
+
 }
